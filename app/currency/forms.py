a566b578548345820005115845166b4ff95ff127
FortiterI <ivansavko2020@gmail.com>
2022-03-20 11:10:42 +0200

    1. Created model Source. 2. Created form SourceForm 3.  Created views source_create, -update, -list, -delete with urls

---
+++ app/currency/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from currency.models import Rate, Source
+
+
+class RateForm(forms.ModelForm):
+    class Meta:
+        model = Rate
+        fields = ("currency_type", "buy", "sale", "source")
+
+
+class SourceForm(forms.ModelForm):
+    class Meta:
+        model = Source
+        fields = ("name", "url")
--- app/currency/models.py
+++ app/currency/models.py
@@ -11,5 +11,10 @@ class Rate(models.Model):
     currency_type = models.CharField(max_length=6)
     buy = models.DecimalField(max_digits=10, decimal_places=2)
     sale = models.DecimalField(max_digits=10, decimal_places=2)
-    created = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
     source = models.CharField(max_length=32)
+
+
+class Source(models.Model):
+    name = models.CharField(max_length=32)
+    url = models.URLField(max_length=200)
