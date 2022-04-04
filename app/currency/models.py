8a6c08d84228edc36fea859d47c973830030b8ed
FortiterI <ivansavko2020@gmail.com>
2022-04-04 22:04:45 +0300

    added view ContactUsCreateView with possibility to send contact us mail

--- app/currency/models.py
+++ app/currency/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 
 class ContactUs(models.Model):
-    email_from = models.CharField(max_length=64)
-    subject = models.CharField(max_length=64)
-    message = models.CharField(max_length=400)
+    created = models.TimeField(auto_now_add=True, )
+    name = models.CharField(max_length=64)
+    reply_to = models.EmailField()
+    subject = models.CharField(max_length=128)
+    body = models.CharField(max_length=1024, default=" ")
+    raw_content = models.TextField()
 
 
 class Rate(models.Model):
